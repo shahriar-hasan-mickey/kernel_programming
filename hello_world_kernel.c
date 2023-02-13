e9a60437241a36b45ca86965f06cb614473978c9
shahriar-hasan-mickey <shahriar.hasan.mickey@g.bracu.ac.bd>
2023-02-13 16:15:28 +0600

    adding the first "hello world" kernel module with the Makefile and some trouble shooting commands which I faced while compiling

---
+++ hello_world_kernel.c
@@ -0,0 +1,13 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void){
+  pr_info("Function loaded to kernel\n");
+  return 0;
+}
+
+void cleanup_module(void){
+  pr_info("Function unloaded from kernel\n");
+}
+
+MODULE_LICENSE("GPL");
